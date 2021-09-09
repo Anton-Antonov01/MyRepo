2e3057503b96bd8e1a97c2b203059f2af7a1fe08
Anton <abantonoff@gmail.com>
2021-09-09 23:47:17 +0300

    Change Hello World

--- TestProj/TestProj/Program.cs
+++ TestProj/TestProj/Program.cs
@@ -6,7 +6,7 @@ namespace TestProj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
         }
     }
 }
